3e564abc7ad0cc9e7b8658a2cfe19e6e13c443b3
Danushka Herath <Danushkaherath96@gmail.com>
2018-09-02 23:40:41 +0530

    initial files

---
+++ resources/views/index.blade.php
@@ -0,0 +1,41 @@
+<html>
+    <head>
+        <title>CRUD Application</title>
+
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/css/materialize.min.css">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    </head>
+    <body>
+        <div class="container">
+        <div class = "card-panel teal lighten-2"><h3 style="text-align: center">CRUD Application</h3></div>
+
+
+        <div class = "card-panel center">
+            <div style="float: left">
+            <a class="btn-floating btn-large waves-effect waves-light red"><i class="material-icons float-left green">add</i></a>
+            </div>
+            <br>
+            <table class="striped" style="margin-top: 50px">
+                <tr>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Address</th>
+                    <th>Telephone</th>
+                    <th>Action</th>
+                </tr>
+                <tr>
+                    <td>Kamal</td>
+                    <td>Perera</td>
+                    <td>Colombo</td>
+                    <td>0717705526</td>
+                    <td>
+                        <button class="btn btn-small blue">Edit</button>
+                        <button class="btn btn-small red">Delete</button>
+                    </td>
+                </tr>
+            </tabel>
+        </div>
+        </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/js/materialize.min.js"></script>
+    </body>
+</html>
